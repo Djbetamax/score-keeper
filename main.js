f4cb47d9fda7bc1ab55ef15193871e0203b191ed
djbetamax <hugots23@gmail.com>
2022-11-20 00:21:48 -0300

    refactor: adjust nest and space on code, separate functions and use callback

--- main.js
+++ main.js
@@ -14,37 +14,35 @@ const reset = function () {
 		score.children[1].style.color = "rgba(0, 0, 0, 0.8)";
 		btnOne.classList.remove('disabled');
 		btnTwo.classList.remove('disabled');
-		btnReset.classList.add("disabled")
+		btnReset.classList.add("disabled");
+	}
+}
+
+const showWinner = () => {
+	if (score.children[0].innerHTML > score.children[1].innerHTML) {
+		score.children[0].style.color = "rgba(15, 141, 12, 1)";
+		score.children[1].style.color = "rgba(255, 12, 12, 1)";
+	} else {
+		score.children[1].style.color = "rgba(15, 141, 12, 1)";
+		score.children[0].style.color = "rgba(255, 12, 12, 1)";
+	}
+}
+
+const playingTo = () => {
+	if (score.children[0].innerHTML == firstTo.value || score.children[1].innerHTML == firstTo.value) {
+		btnOne.classList.add('disabled');
+		btnTwo.classList.add('disabled');
+		showWinner();
 	}
 }
 
 const addPoint = index => {
 	score.children[index].innerHTML++;
 	btnReset.classList.remove("disabled");
-
-	if (index == 0) {
-		score.children[0].style.color = "rgba(21, 131, 248, 1)"
-	} else {
-		score.children[1].style.color = "rgba(15, 141, 15, 1)"
-	}
+	playingTo();
 };
 
-const untilNum = () => {
-	if (score.children[0].innerHTML == firstTo.value || score.children[1].innerHTML == firstTo.value) {
-		btnOne.classList.add('disabled');
-		btnTwo.classList.add('disabled');
-	}
-}
-
 // * Listeners
-btnOne.addEventListener('click', () => {
-	addPoint(0);
-	untilNum();
-})
-
-btnTwo.addEventListener('click', () => {
-	addPoint(1);
-	untilNum();
-})
-
-btnReset.addEventListener('click', reset);
+btnOne.addEventListener("click", () => addPoint(0));
+btnTwo.addEventListener("click", () => addPoint(1));
+btnReset.addEventListener("click", () => reset());
